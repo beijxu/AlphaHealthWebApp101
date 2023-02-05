103babf92d56efff1214cd332fd46a3db885e9ed
Beijie <beijie@amazon.com>
2023-02-05 00:25:41 -0800

    Add Material UI dependency, and upgrade react to 18 to resolve dependency conflict
    I also added an insight page. Not completely working yet.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import LayoutDefault from './layouts/LayoutDefault';
 
 // Views 
 import Home from './views/Home';
+import InsightsMain from './views/insights/Main';
 
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -37,6 +38,7 @@ const App = () => {
       children={() => (
         <Switch>
           <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          <AppRoute exact path="/insights" component={InsightsMain} layout={LayoutDefault} />
         </Switch>
       )} />
   );
